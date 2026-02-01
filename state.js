561f8b8b893b695f9b5bd42e90100151b3a74023
HungryHuman-Tech <suryabhambi@icloud.com>
2026-02-01 11:03:19 -0800

    Refactor state.js to improve formatting and remove defaults

--- state.js
+++ state.js
@@ -6,10 +6,11 @@ export let state = {
     lon: "",
     userLimitDist: 15000,
     openNowOnly: true,
-    highRatedOnly: true, // Default to true
-    minRating: 4.0,      // Default threshold
     blacklist: new Set(),
     history: []
 };
 
-export const forbiddenKeywords = ['hotel', 'inn', 'suites', 'market', 'wholesale', 'grocery', 'liquor', 'supermarket', 'convenience', 'fish market'];
+export const forbiddenKeywords = [
+    'hotel', 'inn', 'suites', 'market', 'wholesale', 
+    'grocery', 'liquor', 'supermarket', 'convenience', 'fish market'
+];
